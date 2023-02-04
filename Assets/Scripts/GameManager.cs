148febb76ac9cfb8bad70d44a25f441c6a229551
Ceck <andrea.cecovig@gmail.com>
2023-02-04 21:12:15 +0100

    * highscore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoSingleton<GameManager>
 {
+    [SerializeField] private TextMeshProUGUI _highscoreLabel;
+
+    private int _highscore;
+
+    public void UpdateHighScore()
+    {
+        _highscore = EnemyManager.Instance.SpawnRound * EnemyManager.Instance.SpawnRound * 10 + EnemyManager.Instance.KillCount * 100;
+        _highscoreLabel.text = "Highscore: " + _highscore;
+    }
+
     public void Restart()
     {
         SceneManager.LoadScene(0);
