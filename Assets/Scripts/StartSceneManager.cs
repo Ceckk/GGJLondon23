5db47b923c5842af37ead187e397aeabb8c64eff
Ceck <andrea.cecovig@gmail.com>
2023-02-05 14:34:57 +0100

    * first scene logic

--- Assets/Scripts/StartSceneManager.cs
+++ Assets/Scripts/StartSceneManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartSceneManager : MonoBehaviour
@@ -8,41 +10,70 @@ public class StartSceneManager : MonoBehaviour
     [SerializeField] private Image _image;
     [SerializeField] private Sprite[] _startFrames;
 
+    [SerializeField] private GameObject[] _pages;
+
     private int _pageIndex = 0;
+    private int _menuIndex = 0;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (_pageIndex == 0)
         {
-            return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (_pageIndex > 0)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                _pageIndex--;
+                switch (_menuIndex)
+                {
+                    case 0:
+                        SceneManager.LoadScene(1);
+                        break;
+                    case 1:
+                        ChangePage(1);
+                        break;
+                    case 2:
+                        ChangePage(2);
+                        break;
+                }
+                return;
             }
-            else
+
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
             {
-                _pageIndex = _startFrames.Length - 1;
+                if (_menuIndex > 0)
+                {
+                    _menuIndex--;
+                }
+                else
+                {
+                    _menuIndex = _startFrames.Length - 1;
+                }
+
+                _image.sprite = _startFrames[_menuIndex];
             }
 
-            _image.sprite = _startFrames[_pageIndex];
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                _menuIndex++;
+                _menuIndex = _menuIndex % _startFrames.Length;
+
+                _image.sprite = _startFrames[_menuIndex];
+            }
         }
-
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        else
         {
-            _pageIndex++;
-            _pageIndex = _pageIndex % _startFrames.Length;
+            if (Input.anyKeyDown)
+            {
+                ChangePage(0);
+            }
+        }
+    }
 
-            _image.sprite = _startFrames[_pageIndex];
+    private void ChangePage(int index)
+    {
+        _pageIndex = index;
+
+        for (int i = 0; i < _pages.Length; i++)
+        {
+            _pages[i].SetActive(i == index);
         }
     }
 }
